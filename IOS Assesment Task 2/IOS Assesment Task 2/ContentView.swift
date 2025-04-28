fa5b31fec2fc6d78d59d1cf6fdb7baa3d09ccb6b
Ali B <30826345+PrinceAlii@users.noreply.github.com>
2025-04-28 22:04:26 +1000

    Basics & Boilerplate

---
+++ IOS Assesment Task 2/IOS Assesment Task 2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IOS Assesment Task 2
+//
+//  Created by Ali bonagdaran on 24/4/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IOS Assesment Task 2/IOS Assesment Task 2/IOS_Assesment_Task_2App.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_Assesment_Task_2App.swift
+//  IOS Assesment Task 2
+//
+//  Created by Ali bonagdaran on 28/4/2025.
+//
+
+import SwiftUI
+
+@main
+struct IOS_Assesment_Task_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
